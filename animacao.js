9eb8b51fcc342816a401c917b0a7ea5e7e758bd0
rodrigomaruya <109206973+rodrigomaruya@users.noreply.github.com>
2024-01-06 13:44:18 +0900

    update geral

--- animacao.js
+++ animacao.js
@@ -1,28 +1,22 @@
-// const animacao=document.getElementById("animacao")
-// const divAnimacao=document.getElementById("divAnimacao")
-// let altura=animacao.offsetHeight-150
-// let largura=animacao.offsetWidth-150
-// let cima=0
-// let baixo=0
-// const arr1=["baleia","bichinho_Verde","dinossauro","kirby","lula","patinho","pato","pokebola","sorvete","ursinho","vaca","pintinho"]
-// const div=document.createElement("div")
-// div.setAttribute("class","divAnimacao")
-// animacao.appendChild(div)
-// const img=document.createElement("img")
-// img.setAttribute("class","imgRedondo")
-// div.appendChild(img)
-// const criardiv=()=>{
-//     imgArr=Math.floor(Math.random()*arr.length)
-//     cima=Math.floor(Math.random()*altura)
-//     baixo=Math.floor(Math.random()*largura)
+const animacao=document.getElementById("animacao")
+const divAnimacao=document.getElementById("divAnimacao")
 
-//     img.setAttribute("src","/img_croche/"+arr1[imgArr]+".jpg")
-//     div.style=`position:absolute;top:${cima}px;left:${baixo}px;`
-    
-// }
+const arr1=["baleia","bichinho_Verde","dinossauro","kirby","lula","patinho","pato","pokebola","sorvete","ursinho","vaca","pintinho"]
+
+const div=document.createElement("div")
+div.setAttribute("class","divAnimacao")
+animacao.appendChild(div)
+const img=document.createElement("img")
+img.setAttribute("class","imgRedondo")
+div.appendChild(img)
+const criardiv=()=>{
+    imgArr=Math.floor(Math.random()*arr1.length)
+    img.setAttribute("src","/img_croche/"+arr1[imgArr]+".jpg")
+}
 
 
-// const anima=()=>{
-//     criardiv()
-// }
-// setInterval(anima,1500)
+const anima=()=>{
+    criardiv()
+}
+anima()
+setInterval(anima,1500)
